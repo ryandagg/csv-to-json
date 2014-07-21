2868697f92e4e86e17c2a2b90a5046f533617ccd
ryandagg <ryan.dagg@gmail.com>
2014-07-21 13:58:49 -0600

    Actual final. Forgot to create new file and add text in previous commit.

--- csv-to-json.js
+++ csv-to-json.js
@@ -16,29 +16,27 @@ $ node csv-to-json.js input.csv output.js*/
 
 var fs = require('fs');
 
+// get text of input file which is the 2nd command line arguement
 var inputText = fs.readFileSync(process.argv[2], 'utf-8');
 
+// create array equal with each element equal to each line of csv file
 var inputArrayByLine = inputText.split(/\n/g);
-console.log("inputArrayByLine: ", inputArrayByLine);
+// console.log("inputArrayByLine: ", inputArrayByLine);
 
+// get tiles of columns to be used for creating row object
 var columnHeads = inputText.slice(0, inputText.search(/\n/)).split(',');
-console.log("columnHeads: ", columnHeads);
+// console.log("columnHeads: ", columnHeads);
 
 var totalRows = inputText.match(/\n/g).length + 1;
-console.log("totalRows: ", totalRows);
+// console.log("totalRows: ", totalRows);
 
 var totalColumns = columnHeads.length;
-console.log("totalColumns: ", totalColumns);
+// console.log("totalColumns: ", totalColumns);
 
 
 var output = [];
 
-// var Row = function(params) {
-// 	for(var i in params) {
-// 		this.i = params.i
-// 	}
-// }
-
+// iterate through each cell of csv and build an object to push to final output
 for(var y = 1; y < totalRows; y++) {
 	var line = inputArrayByLine[y].split(',');
 	var row = {};
@@ -48,6 +46,8 @@ for(var y = 1; y < totalRows; y++) {
 	output.push(row);
 }
 
-console.log(output);
+// console.log(output);
 
-return output;
+// create a new file and write the ouptut to it. Location is taken from the 3rd argument entered from console.
+fs.openSync(process.argv[3], 'w');
+fs.writeFileSync(process.argv[3], output);
